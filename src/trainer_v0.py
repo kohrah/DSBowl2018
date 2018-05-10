e034eef96d60c56121ea3b98e79c4c6815de78f5
kohrah <av.korekov@gmail.com>
2018-05-10 21:41:31 +0300

    Add files via upload

--- src/trainer_v0.py
+++ src/trainer_v0.py
@@ -105,11 +105,11 @@ class Trainer(object):
                 train_gen = self.get_generator(is_train=True, dataset=train_data)
                 val_gen = self.get_generator(is_train=False, dataset=val_data)
 
-                model_ = zoo_unet_K.get_unet_512_spartial
+                # model_ = zoo_unet_K.get_unet_512_spartial
                 # model = model_(input_shape=(self.input_size, self.input_size, self.n_channels), classes=self.classes,
                 #                batch_norm=self.batch_norm, n_filters=16, dropout_val=self.dropout)
-                model = model_(UNET_INPUT=self.input_size, classes=self.classes,
-                               dropout_val=self.dropout, batch_norm=self.batch_norm)
+                model = self.model(UNET_INPUT=self.input_size, classes=self.classes,
+                                   dropout_val=self.dropout, batch_norm=self.batch_norm)
                 callback = KFold_cbk(model, fold)
                 fold_checkpoint_name = 'model_at_fold_BN_%d.h5' % fold
                 for epochs, lr, iter_n in self.lr_schedule:
@@ -147,68 +147,3 @@ class Trainer(object):
                     check_id = iter_n
                 fold += 1
             print('Finished!')
-#
-# # train
-# import zoo_losses_K
-# import zoo_unet_K
-#
-# phase = 'train'
-# batch_size = 24
-# classes = 2
-# batch_norm = True
-# dropout = 0.0
-# unet_input = 224
-# norm_type = 'mean_std'
-# predict_seeds = False
-# if predict_seeds:
-#     classes = 2
-# n_folds = 3
-# exclude_folds = [1, 2]
-# lr_sched = [[10, 0.0003, '1'], [10, 0.0002, '2'], [15, 0.00015, '3'],
-#             [90, 0.0001, '4']]#, [20, 0.0003, '5']]
-# # lr_sched = [[1, 0.0001, '1'], [1, 0.0005, '2'], [1, 0.0002, '3'],
-# #             [1, 0.0001, '4'], [1, 0.00007, '5']]
-# use_model = zoo_unet_K.get_VGGunet_bnd_v3
-# loss = zoo_losses_K.dice_coef_weighted_one_class #dice_coef_and_binary_loss
-# metrics = zoo_losses_K.dice_coef
-#
-# seed = 66 #980
-#
-# if os.name == 'nt':
-#     GLOBAL_PATH = 'E:\Datasets\\dsbowl2018\\'
-# else:
-#     GLOBAL_PATH = '/home/nosok/datasets/dsBowl2018/'
-#
-# if os.name != 'nt':
-#     use_multiprocessing = True
-# else:
-#     use_multiprocessing = False
-#
-# if phase == 'train':
-#     trainer = Trainer(model=use_model, optimizer=Nadam,
-#                       batch_size=batch_size, input_size=unet_input, norm_function=norm_type,
-#                       loss=loss, classes=classes,
-#                       predict_seeds=predict_seeds,
-#                       lr_schedule=lr_sched,
-#                       n_folds=n_folds, exclude_folds=exclude_folds,
-#                       seed=seed,
-#                       use_multiprocessing=use_multiprocessing,
-#                       batch_norm=batch_norm,
-#                       dropout=dropout)
-#     trainer.train()
-#
-# if phase == 'test':
-#     Test_dataset = BowlDataset(train_fraction=1, seed=1, phase='test', dataset_return_ids_only=True)
-#     test_data = Test_dataset.test_data
-#     predictor = Predictor(phase='test', dataset=test_data, model=use_model, model_input_size=unet_input,
-#                           path=GLOBAL_PATH,
-#                           predict_on_crops=False,
-#                           n_classes=classes, normalize=norm_type,
-#                           n_folds=n_folds, exclude_folds=exclude_folds,
-#                           nb_tta_times=2,
-#                           mask_threshold=0.5, seed_threshold=0.6, seed_min_size=4,
-#                           debug=True,
-#                           tf_device="/gpu:0",
-#                           dataset_return_ids_only=True)
-#     predictor.predict()
-#
